f03e812fabb1e49d09d10b9634672545fa226b34
youngminshin <cntmin81@gmail.com>
2022-03-21 09:50:41 +0900

    minboard first commit

---
+++ src/main/java/io/github/cntmin81/minboard/MinboardApplication.java
@@ -0,0 +1,13 @@
+package io.github.cntmin81.minboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/cntmin81/minboard/MinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cntmin81.minboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
